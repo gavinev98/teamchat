27c8d802f5b6129c93823f87ad1054d598f469a1
Gavin Everett <gavin.everett48@hotmail.com>
2021-02-27 15:24:09 +0000

    passing existing props of chat feed to component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
-
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
 
@@ -9,7 +9,8 @@ const App = () => {
        projectID='f135e1d0-8999-4545-b622-3954a72de173'
        userName='gavinev98'
        userSecret='kerry1234'
-       
+       height='100vh'
+       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
        />
 
 
