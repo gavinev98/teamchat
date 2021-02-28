813e85558477bd6555e0b71f239d6fc32895fbab
Gavin Everett <gavin.everett48@hotmail.com>
2021-02-28 17:14:58 +0000

    rendering the message.

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 
 const TheirMessage = ({ message, lastMessage}) => {
 
-    if(message?.attatchements?.length > 0) {
-            return (
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+
+
+    return (
+        <div className="message-row">
+       {isFirstMessageByUser && (
+           <div 
+                className="message-avatar"
+                style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+           />
+       )} 
+        {message?.attatchements?.length > 0
+           ?(
                 <img 
                 src={message.attatchements[0].file}
                 alt="message-attatchement"
                 className="message-image"
                 style={{ float: 'right'}}
                 />
-            );
-    }
-
-    return (
-        <div>
-          
+            ) : (
+                <div className="message" style={{ float: 'right', marinRight: '18px', color: 'white' , backgroundColor : '#3B2A50'}}>
+                {message.text}    
+             </div>
+            )
+        }   
         </div>
     );
 };
