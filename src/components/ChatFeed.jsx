a1df0d023fac98b06d280989f1f3190154e7c4c2
Gavin Everett <gavin.everett48@hotmail.com>
2021-02-28 10:46:09 +0000

    acessing the properties through custom chat class.

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-
-
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
 const ChatFeed = (props) => {
-console.log(props);
+
+    const { chats, activeChat, userName, messages} = props;
+
+    //finding the current active chat,
+    const chatActive = chats && chats[activeChat];
 
     return (
         <div>
-            
+          
         </div>
     );
 };
