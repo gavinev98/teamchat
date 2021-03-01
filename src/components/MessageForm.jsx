610db566730751a0b0cbf0465c2a54a493bce254
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-01 10:20:25 +0000

    setting up structure of input form for sending msg

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useState } from 'react';
 
 const MessageForm = () => {
+
+    //creating state property for input textbox
+    const [value , setValue] = useState('');
+ 
+
+    //handling the submit of sending a message.
+    const handleSumit = ({ }) => {
+
+    }
+
+    //handling the change of a message.
+    const handleChange = () => {
+
+    }
+
+
     return (
-        <div>
-                MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+             className="message-input"
+             placeholder="Send a message!"
+             value={value}
+             onChange={handleChange}
+            />
+        </form>
     );
 };
 
