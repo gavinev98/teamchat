396ec5911fa9070f708af6984b899e2ffa99d1c7
Gavin Everett <gavin.everett48@hotmail.com>
2021-03-03 11:51:43 +0000

    setting up username field.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import axios from 'axios';
 
 
 const LoginForm = () => {
@@ -10,8 +10,23 @@ const LoginForm = () => {
     //state for the password field
     const [password, setPassword] = useState('');
 
+
+    //handle login
+    const handleSubmit = () => {
+
+
+    }
+
     return (
-        <div>
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onchange={(e) => setUsername(e.target.value)}  />
+
+
+                </form>
+            </div>
             
         </div>
     );
